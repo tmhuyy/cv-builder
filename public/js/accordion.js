b4bb5b69be765c00b80a8029843d19cadb31f394
Huy Tran <tmhuyy.itiu@gmail.com>
2022-04-17 22:35:46 +0700

    finish fix bugs for accordion with dynamic form field

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -1,13 +1,23 @@
-const accordionBtn = document.querySelectorAll(".accordion");
+const accordion = document.querySelector("#more-form1");
 
-for (let i = 0; i < accordionBtn.length; i++) {
-    accordionBtn[i].addEventListener("click", function () {
-        accordionBtn[i].classList.toggle("change");
-        let panel = accordionBtn[i].nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+accordion.addEventListener("click", function (e) {
+    const form = e.target;
+    form.classList.toggle("change");
+    let panel = form.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+})
+// for (let accordionBtn of accordionBtns) {
+//     accordionBtn.addEventListener("click", function () {
+//         this.classList.toggle("change");
+//         let panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//             panel.style.maxHeight = null;
+//         } else {
+//             panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }
